3c9bc7f44172ff0ff7197666420ee57b68006e1c
Savith Kandegedara <savith2001523@gmail.com>
2024-03-19 17:35:14 +0530

    added homepage

---
+++ Front end/Final/screens/Tips.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {View, Text, SafeAreaView,} from 'react-native';
+
+const Tips = (navigation) => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Text>Tips Screen</Text>
+    </View>
+  )
+}
+
+export default Tips
